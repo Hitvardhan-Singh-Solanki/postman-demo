d21e7eb8c55fadd901d5d2d092b2c6c2ceab8241
Hitvardhan Singh Solanki <hitvardhan19@gmail.com>
2020-04-01 13:17:33 +0530

    room config

--- src/config/socket.js
+++ src/config/socket.js
@@ -1,6 +1,7 @@
 import socketio from 'socket.io';
 import http from 'http';
 import { addUser, removeUser, getAllActiveUsers } from '../sockets/users';
+import { startTime } from '../utils';
 
 const MAIN_ROOM = 'MAIN_ROOM';
 
@@ -10,20 +11,22 @@ export default (expressApp, sessionMiddleware) => {
   io.use((socket, next) => {
     sessionMiddleware(socket.request, {}, next);
   }).on('connection', socket => {
+    console.log(`${startTime()} new client connected`, socket.id);
     socket.on('join', currentUser => {
       socket.join(MAIN_ROOM);
       socket.emit('joined', {
-        text: `Welcome to the ${MAIN_ROOM}`,
+        text: `Welcome to the club!`,
         ...currentUser
       });
       addUser({ ...currentUser, socketid: socket.id });
       socket.broadcast.to(MAIN_ROOM).emit('USER_JOINED', { currentUser });
       io.in(MAIN_ROOM).emit('ROOM_DATA', { users: getAllActiveUsers() });
     });
-    socket.on('disconnect', data => {
-      // console.log('-----+++++++', socket.id);
-      // const user = removeUser(socket.id);
-      // io.to(MAIN_ROOM).emit('USER_LEFT', { id: socket.id });
+    socket.on('disconnect', () => {
+      console.log(`client disconnected`, socket.id);
+      const users = removeUser(socket.id);
+      io.to(MAIN_ROOM).emit('USER_LEFT', { users });
+      io.in(MAIN_ROOM).emit('ROOM_DATA', { users: getAllActiveUsers() });
     });
   });
   return server;
--- src/utils/index.js
+++ src/utils/index.js
@@ -3,3 +3,15 @@ export const userResponsObject = ({ email, _id, image }) => ({
   image,
   _id
 });
+
+const checkTime = i => {
+  return i < 10 ? '0' + i : i;
+};
+
+export const startTime = () => {
+  const today = new Date(),
+    h = checkTime(today.getHours()),
+    m = checkTime(today.getMinutes()),
+    s = checkTime(today.getSeconds());
+  return h + ':' + m + ':' + s;
+};
